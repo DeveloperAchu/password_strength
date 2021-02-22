cb394879bf4f7f8897874837fd46645603d8d12a
Achu <achuunnikrishnan1@gmail.com>
2021-02-22 20:17:55 +0000

    home page documentation added

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:password_strength/widgets/color_bar.dart';
 
 import '../static_files/password_strength.dart';
 
 import '../widgets/text_widget.dart';
 import '../widgets/password_field.dart';
+import '../widgets/color_bar.dart';
 import '../widgets/password_strength_text.dart';
 
 import '../utils/colors.dart';
 import '../utils/assets.dart';
+
+/*
+  This screen is the main screen of the app.
+*/
 
 class HomePage extends StatefulWidget {
   @override
@@ -17,48 +21,116 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  /* 
+    A text editing controller is declared and this will watch the password
+    entered by the user.
+    This is declared as final because this won't change later during the entire
+    lifecycle of the app.
+  */
   final _passwordController = TextEditingController();
+
+  /*
+    A color variable is declared to control the text field cursor color and the
+    underline gradient end color.
+  */
   Color _passwordCursorColor = STRONG_TEXT_COLOR;
+
+  /*
+    A string is declared to show the strength of the password. The different
+    strengths are declared in a static file. The strength can be none, weak,
+    medium, and strong. Since the user is not engaging with the input field,
+    initially strength is declared as none.
+  */
   String _passwordStrength = PasswordStrength.none;
+
+  /*
+    A boolean variable is declared to toggle the visibility of the password
+    strength suffix icon. Since the user is not engaging with the input field,
+    initially icon is hidden.
+  */
   bool _showPasswordIcon = false;
+
+  /* 
+    A string variable is declared to dynamically change the url of the icon. The
+    icon can be a success icon or an error icon. The url to these icon assets
+    are declared in the assets file in the utils folder.
+  */
   String _iconAsset;
 
+  /*
+    In the initState function, we add a listener function to the password text
+    controller to handle the text entered by the user.
+  */
   @override
   void initState() {
     super.initState();
     _passwordController.addListener(_passwordTextChangeListener);
   }
 
+  /*
+    This is the listener function of the text editing controller.
+    
+    This function checks the number of characters in the string. If it is
+    greater than or equal to 7, the password is marked as a strong password. If 
+    the length is in between 4 and 7, it is marked as a medium password. If it
+    is less than 4, it is marked as weak password.
+  */
   void _passwordTextChangeListener() {
-    Color newColor;
+    Color updatedCursorColor;
     String strength;
     String icon;
 
     if (_passwordController.text.length >= 7) {
-      // strong password
+      /* 
+        strong password.
+        The strength of the password is marked as strong.
+        The updated cursor color and the gradient end color is marked as a shade
+        of green color.
+        A success icon is attached as the suffix of the password field.
+      */
       strength = PasswordStrength.strong;
-      newColor = STRONG_TEXT_COLOR;
+      updatedCursorColor = STRONG_TEXT_COLOR;
       icon = SUCCESS_ICON;
     } else if (_passwordController.text.length > 4) {
-      // medium strong password
+      /* 
+        medium password.
+        The strength of the password is marked as medium.
+        The updated cursor color and the gradient end color is marked as a shade
+        of red color.
+        An error icon is attached as the suffix of the password field.
+      */
       strength = PasswordStrength.medium;
-      newColor = WEAK_TEXT_COLOR;
+      updatedCursorColor = WEAK_TEXT_COLOR;
       icon = ERROR_ICON;
     } else {
-      // weak password
+      /* 
+        weak password.
+        The strength of the password is marked as medium.
+        The updated cursor color and the gradient end color is marked as a shade
+        of red color.
+        An error icon is attached as the suffix of the password field.
+      */
       strength = PasswordStrength.weak;
-      newColor = WEAK_TEXT_COLOR;
+      updatedCursorColor = WEAK_TEXT_COLOR;
       icon = ERROR_ICON;
     }
 
+    /* 
+      All the above changes are rebuit by invoking the setState function
+    */
     setState(() {
       _passwordStrength = strength;
-      _passwordCursorColor = newColor;
+      _passwordCursorColor = updatedCursorColor;
       _showPasswordIcon = true;
       _iconAsset = icon;
     });
   }
 
+  /*
+    This function clears all the variables to the default or initial value. This
+    function is invoked when the user tap on the screen. The changes are rebuilt
+    by invoking the setState function.
+  */
   void _clearFields() {
     FocusScope.of(context).unfocus();
     setState(() {
